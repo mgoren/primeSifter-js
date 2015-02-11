b2bb5810c6d9756edcbf8240433b6da304536636
Mike Goren <mgoren@mortalwombat.net>
2015-02-10 16:42:11 -0800

    add fun css styling

--- js/scripts.js
+++ js/scripts.js
@@ -29,4 +29,61 @@ $(document).ready(function() {
 
     event.preventDefault();
   });
+
+  var extraUX = false;
+
+  if ( extraUX ) $('form').addClass('more-ux');
+
+  var checkallreq = function () {
+    var _reqlen = $('input[required]').length,
+        _passlen = $('.fields li.pass').length;
+
+    if ( parseInt($('input[required]').val()) > 1 ) {
+      $('form button').removeAttr('disabled');
+    } else {
+      $('form button').attr('disabled', 'disabled');
+    }
+  };
+
+  var checkfield = function (e) {
+    var _basic = ['first_name', 'last_name'],
+        $_field = $('.fields input').filter('[name="' + e + '"]'),
+        $_parent = $_field.parent();
+
+    if ( $.inArray(_basic, e) ) {
+      if ( $_field.val().length > 0 && parseInt($_field.val()) >= 2 ) {
+        $_parent.removeClass('fail');
+        $_parent.addClass('pass');
+      } else {
+        $_parent.removeClass('pass');
+        $_parent.addClass('fail');
+      }
+    }
+
+    checkallreq();
+  };
+
+  $('.fields input[required]')
+    .each(function () {
+      $_input = $(this).attr('name');
+      $(this).parent().addClass('required fail');
+
+      checkfield($_input);
+    } )
+
+    .change(function () {
+      $_input = $(this).attr('name');
+
+      checkfield($_input);
+    } );
+
+
+  $('form.more-ux .fields input')
+    .on('keypress', function () {
+      $(this).addClass('editting');
+      $(this).on('blur', function () {
+        $(this).removeClass('editting');
+      } )
+    });
+
 });
